cc376559d851d07b8643f01fc3d7e61c82cde883
unknown <hunny.rahi55@gmail.com>
2021-12-18 16:33:24 +0530

    LifeCycle

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,23 +1,38 @@
 <template>
-<div class="block">
-    Click Me
-</div>
+  <div class="block" v-if="showBlock">Click Me</div>
 </template>
 
 <script>
 export default {
-    props : ['delay']
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log("Componenet Mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+  updated() {
+    console.log("Component Updated");
+  },
+  unmounted() {
+    console.log("Component Unmounted");
+  },
 };
 </script>
 
 <style>
-.block{
-    width: 400px;
-    border-radius: 30px;
-    background-color: purple;
-    color: white;
-    text-align: center;
-    padding: 80px 10px;
-    margin: auto;
+.block {
+  width: 400px;
+  border-radius: 30px;
+  background-color: purple;
+  color: white;
+  text-align: center;
+  padding: 80px 10px;
+  margin: auto;
 }
 </style>
