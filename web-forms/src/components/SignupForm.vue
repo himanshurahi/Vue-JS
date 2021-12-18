196b21ec6872cb4115ea889d01c40e5972b6e010
unknown <hunny.rahi55@gmail.com>
2021-12-18 17:15:59 +0530

    Web Forms TwoWay DataBinding

---
+++ web-forms/src/components/SignupForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>Web Forms</h1>
+    <div class="row" style="justify-content: center">
+      <div class="col-6">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1" style="float: left"
+              >Email address</label
+            >
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              v-model="email"
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1" style="float: left">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              v-model="password"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+          <p>Email : {{ email }}</p>
+          <p>Password : {{ password }}</p>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
