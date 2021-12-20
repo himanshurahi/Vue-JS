a88fe226ce5597c616594e12006b973bea398abd
unknown <hunny.rahi55@gmail.com>
2021-12-20 11:38:01 +0530

    Added JSON Server and Changed API

---
+++ project_planner/src/components/SIngleProject.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>HImanshurhi</div>
+</template>
+
+<script>
+export default {
+  name: "SingleProject",
+};
+</script>
+
+<style>
+</style>
--- project_planner/src/views/Home.vue
+++ project_planner/src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="project in projects" :key="project.id">
+        {{ project.title }}
+        <div class="icons">
+          <i class="fa fa-pencil m-2" aria-hidden="true"></i>
+          <i
+            @click="deleteProject(project.id)"
+            class="fa fa-trash m-2"
+            aria-hidden="true"
+          ></i>
+          <i class="fa fa-check m-2" aria-hidden="true"></i>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleProject from "../components/SIngleProject.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    deleteProject(id) {
+      fetch("http://localhost:3000/projects/" + id, {
+        method: "DELETE",
+      }).then(() => {
+        this.projects = this.projects.filter((project) => project.id != id);
+      });
+    },
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
+
+<style scoped>
+i {
+  cursor: pointer;
+}
+</style>
