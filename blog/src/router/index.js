94fbe1767f02739cfe7005f06b864604acd61566
unknown <hunny.rahi55@gmail.com>
2021-12-20 18:01:06 +0530

    Form Added

--- blog/src/router/index.js
+++ blog/src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
 
 const routes = [
     {
@@ -12,7 +13,13 @@ const routes = [
         name: 'Details',
         component: Details,
         props: true
-    }
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: Create,
+      props: true
+  }
 
 ]
 
---
+++ blog/src/views/Create.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="create">
+    <form action="">
+      <label for="">Title</label>
+      <input type="text" name="" id="" v-model="title" />
+      <label for="">Content</label>
+      <textarea v-model="body"></textarea>
+      <label for="">Tags (Hit Enter to add Tags)</label>
+      <input
+        v-model="tag"
+        @keydown.enter.prevent="handleKeyDown"
+        type="text"
+        name=""
+        id=""
+      />
+      <ul>
+        <li v-for="tag in tags" :key="tag">
+          {{ tag }}
+        </li>
+      </ul>
+      <button>Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const title = ref("");
+    const body = ref("");
+    const tag = ref("");
+    const tags = ref([]);
+
+    const handleKeyDown = () => {
+      if (!tags.value.includes(tag.value)) {
+        // tag.value = tag.value.replace("/s/", "");
+        tags.value.push(tag.value);
+      }
+
+      tag.value = "";
+    };
+
+    return { title, body, tag, handleKeyDown, tags };
+  },
+};
+</script>
+
+<style>
+</style>
