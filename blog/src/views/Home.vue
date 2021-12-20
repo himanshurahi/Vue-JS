6a6d62ca8edb4bedc7f4faa0498b84720049ac59
unknown <hunny.rahi55@gmail.com>
2021-12-20 14:15:54 +0530

    Composition API Using Ref

--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="home">
-    <p>my name is {{ name }} and {{ age }}</p>
+    <p ref="p">my name is {{ name }} and {{ age }}</p>
     <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
+    // this.$refs
+    //this keyword not available here
     console.log("setup");
     let name = "himanshrahi";
     let age = 30;
+    const p = ref(null);
     const handleClick = () => {
-      console.log('clicked');
-    }
-    return { name, age, handleClick };
+      console.log(p.value);
+    };
+    return { name, age, handleClick, p };
   },
 };
 </script>
