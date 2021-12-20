e480d9e805d134a11fd8d4ffd35d40da111845d6
unknown <hunny.rahi55@gmail.com>
2021-12-20 14:10:22 +0530

    Composition API #1

--- blog/src/views/Home.vue
+++ blog/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <p>my name is {{ name }} and {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
@@ -11,7 +12,10 @@ export default {
     console.log("setup");
     let name = "himanshrahi";
     let age = 30;
-    return { name, age };
+    const handleClick = () => {
+      console.log('clicked');
+    }
+    return { name, age, handleClick };
   },
 };
 </script>
