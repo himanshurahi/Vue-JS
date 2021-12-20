2692ef816cc3484f1576f953dfe60911f8819666
unknown <hunny.rahi55@gmail.com>
2021-12-20 14:08:35 +0530

    Composition API

---
+++ blog/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>my name is {{ name }} and {{ age }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    console.log("setup");
+    let name = "himanshrahi";
+    let age = 30;
+    return { name, age };
+  },
+};
+</script>
