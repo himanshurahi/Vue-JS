4b28415ae7ae50f4ff518c4f273b0e0fb5ff3bd5
unknown <hunny.rahi55@gmail.com>
2021-12-21 12:35:19 +0530

    LiveChat Room

---
+++ live_chatroom/src/firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getFirestore, serverTimestamp} from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDL_FbOitxx3ECCRLz8NboPszSIAsJ0noU",
+    authDomain: "vue-database-28211.firebaseapp.com",
+    projectId: "vue-database-28211",
+    storageBucket: "vue-database-28211.appspot.com",
+    messagingSenderId: "342905097457",
+    appId: "1:342905097457:web:6b02aabe186784ba936d50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const firestore = getFirestore();
+const timestamp = serverTimestamp();
+
+
+export {
+    firestore,
+    timestamp
+}
