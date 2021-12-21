68cce1780fe28dc06fbdd8d10acada6dff5807aa
unknown <hunny.rahi55@gmail.com>
2021-12-21 15:19:47 +0530

    Vuex

---
+++ vuex_demo/src/store/index.js
@@ -0,0 +1,77 @@
+import {createStore} from 'vuex'
+
+export default createStore({
+    state: {
+        counter: 0,
+        users: [
+            {
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                        "lat": "-37.3159",
+                        "lng": "81.1496"
+                    }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                }
+            }, {
+                "id": 2,
+                "name": "Ervin Howell",
+                "username": "Antonette",
+                "email": "Shanna@melissa.tv",
+                "address": {
+                    "street": "Victor Plains",
+                    "suite": "Suite 879",
+                    "city": "Wisokyburgh",
+                    "zipcode": "90566-7771",
+                    "geo": {
+                        "lat": "-43.9509",
+                        "lng": "-34.4618"
+                    }
+                },
+                "phone": "010-692-6593 x09125",
+                "website": "anastasia.net",
+                "company": {
+                    "name": "Deckow-Crist",
+                    "catchPhrase": "Proactive didactic contingency",
+                    "bs": "synergize scalable supply-chains"
+                }
+            },
+        ]
+    },
+    getters: {
+        allUsers: state => state.users,
+        counter: state => state.counter
+    },
+
+    actions: {
+        async getUsers({commit}) {
+            const res = await fetch('https://jsonplaceholder.typicode.com/users');
+            const json = await res.json();
+            commit("setUsers", json)
+        },
+
+        addCounter({commit}) {
+            commit('addCounter')
+        }
+    },
+    mutations: {
+        setUsers: (state, users) => (state.users = users),
+        addCounter: (state) => {
+            state.counter = state.counter + 1;
+        }
+    },
+    modules: {}
+})
---
+++ vuex_demo/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <!-- {{ allUsers }} -->
+    <h3>Counter : {{ counter }}</h3>
+    <button @click="addCounter()">Incrase Counter</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "Home",
+  components: {},
+  computed: mapGetters(["allUsers", "counter"]),
+  methods: {
+    ...mapActions(["getUsers", "addCounter"]),
+  },
+  mounted() {
+    this.getUsers();
+  },
+};
+</script>
